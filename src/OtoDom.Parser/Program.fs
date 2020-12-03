035eae78b43f4249338891d7da2286da17c46d87
dominikus1993 <dominikus1910@gmail.com>
2020-12-03 19:06:41 +0100

    add simple parser

--- src/OtoDom.Parser/Program.fs
+++ src/OtoDom.Parser/Program.fs
@@ -4,15 +4,21 @@ open System
 open FSharp.Data
 open FSharp.Control
 
+[<Literal>]
+let tmpUrl = "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/radogoszcz/?search%5Bfilter_float_price%3Ato%5D=400000&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70&search%5Bregion_id%5D=5&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&search%5Bdistrict_id%5D=1502"
 
 // Define a function to construct a message to print
 let loadArticles () =
     asyncSeq {
+        let! document = HtmlDocument.AsyncLoad(tmpUrl)
+        let res = document.Descendants["article"]
         
+        for node in res do
+            yield {| text = node.InnerText() |}
     }
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let message = loadArticles() |> AsyncSeq.toListAsync |> Async.RunSynchronously
+    printfn "Hello world %A" message
     0 // return an integer exit code
