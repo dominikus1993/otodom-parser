cef82bd201f66093fee271f9d11a07f1e4fe4f0a
dominikus1993 <dominikus1910@gmail.com>
2020-12-03 18:51:38 +0100

    small changes

--- src/OtoDom.Parser/Program.fs
+++ src/OtoDom.Parser/Program.fs
@@ -1,10 +1,14 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open FSharp.Data
+open FSharp.Control.AsyncSeq
 
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let loadArticles () =
+    asyncSeq {
+        
+    }
 
 [<EntryPoint>]
 let main argv =
