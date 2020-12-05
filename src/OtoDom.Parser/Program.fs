23ce4c12af6aa083805493ae0638ba33261fbde2
dominikus1993 <dominikus1910@gmail.com>
2020-12-05 08:59:08 +0100

    add parser

--- src/OtoDom.Parser/Program.fs
+++ src/OtoDom.Parser/Program.fs
@@ -3,52 +3,26 @@
 open System
 open FSharp.Data
 open FSharp.Control
+open HtmlAgilityPack
 
 [<Literal>]
 let tmpUrl =
     "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/radogoszcz/?search%5Bfilter_float_price%3Ato%5D=400000&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70&search%5Bregion_id%5D=5&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&search%5Bdistrict_id%5D=1502"
 
-let trim(str: String) =
-    str.Trim()
+let split (separator: char)(str: String) = str.Split(separator)
+let trim (str: String) = str.Trim()
 
 // Define a function to construct a message to print
 let loadArticles () =
     asyncSeq {
-        let! document = HtmlDocument.AsyncLoad(tmpUrl)
-        let res = document.Descendants [ "article" ]
-
+        let web = new HtmlWeb()
+        let! document = web.LoadFromWebAsync(tmpUrl) |> Async.AwaitTask
+        let res = document.DocumentNode.Descendants("article")
         for node in res do
-            let header =
-                node.Descendants()
-                |> Seq.filter (fun x -> x.HasClass("offer-item-header\n"))
-                |> Seq.head
-            let link =
-                node.Descendants [ "a" ]
-                |> Seq.choose (fun x -> x.TryGetAttribute("href"))
-                |> Seq.head
-
-            let price =
-                node.Descendants()
-                |> Seq.filter (fun x -> x.HasClass("offer-item-price"))
-                |> Seq.map(fun x -> x.InnerText() |> trim)
-                |> Seq.head
-                
-            let pricePerMeter =
-                node.Descendants()
-                |> Seq.filter (fun x -> x.HasClass("offer-item-price-per-m"))
-                |> Seq.map(fun x -> x.InnerText() |> trim)
-                |> Seq.head
-
-            let area =
-                node.Descendants()
-                |> Seq.filter (fun x -> x.HasClass("offer-item-area"))
-                |> Seq.map(fun x -> x.InnerText() |> trim)
-                |> Seq.head
-            yield
-                {| pricePerMeter = pricePerMeter
-                   link = link.Value()
-                   price = price
-                   area = area |}
+            let href = node.Descendants("a") |> Seq.map(fun x -> x.GetAttributeValue("href", null)) |> Seq.head
+            let detailsNode = node.ChildNodes |> Seq.filter(fun x -> x.HasClass("offer-item-details")) |> Seq.head
+            let details = detailsNode.InnerText |> split('\n') |> Array.map(fun x -> x |> trim) |> Array.filter(fun x -> x |> String.IsNullOrEmpty |> not)
+            yield {| details = details; url = href |}
     }
 
 [<EntryPoint>]
