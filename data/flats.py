57d3ba774001f2b0d3f7b990a44c68271f8cc0b4
dominikus1993 <dominikus1910@gmail.com>
2020-12-09 18:55:03 +0100

    add

--- data/flats.py
+++ data/flats.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import datetime
 
-def datetimeRange(): 
+def getCsvFiles(): 
     today = datetime.datetime.today().strftime("%Y-%m-%d")
-    return pd.date_range(start="2020-12-08",end=today).to_native_types().tolist()
+    dates = pd.date_range(start="2020-12-08",end=today).to_native_types().tolist()
+    return list(map(lambda d: f'otodom-{d}.csv', dates))
 
 #file = pd.read_csv("./otodom-09-12-2020.csv")
 
