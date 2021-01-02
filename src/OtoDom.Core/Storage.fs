9e8f88da1c87eb9b0ff30daa67d09358d7ba45ef
dominikus1993 <dominikus1910@gmail.com>
2021-01-02 19:53:45 +0100

    make offers unique

--- src/OtoDom.Core/Storage.fs
+++ src/OtoDom.Core/Storage.fs
@@ -7,21 +7,38 @@ open OtoDom.Core.Types
 open OtoDom.Core.Utils
 
 module CsvStorage =
-    let FirstRow = "Opis,Dzielnica,Ilość pokoi,Cena,Powierzchnia,Cena za m2,Data,Link do ogłoszenia"
-    
-    let private row(elem: Offer) =
+    let FirstRow =
+        "Opis,Dzielnica,Ilość pokoi,Cena,Powierzchnia,Cena za m2,Data,Link do ogłoszenia"
+
+    let private row (elem: Offer) =
         let date = DateTime.Now.Date.ToString("dd-MM-yyyy")
-        $"{elem.Description},{elem.District},{elem.Rooms},{elem.Price},{elem.Area},{elem.PricePerMeter},{date},{elem.Href}"
-    let private createCsv(elements: Offer seq) =
-        let b = StringBuilder() |> StringBuilder.appendLine FirstRow
-        elements |> Seq.map(row) |> Seq.fold(fun builder x -> builder |> StringBuilder.appendLine x) b |> StringBuilder.toString
-           
+
+        $"{elem.Description},{elem.District},{elem.Rooms},{elem.Price},{elem.Area},{elem.PricePerMeter},{date},{
+                                                                                                                    elem.Href
+        }"
+
+    let private createCsv (elements: Offer seq) =
+        let b =
+            StringBuilder()
+            |> StringBuilder.appendLine FirstRow
+
+        elements
+        |> Seq.map (row)
+        |> Seq.fold (fun builder x -> builder |> StringBuilder.appendLine x) b
+        |> StringBuilder.toString
+
     let store (elements: Offer seq) =
         async {
             let date = DateTime.Now.Date.ToString("yyyy-MM-dd")
             let fileName = $"otodom-{date}.csv"
-            let csv = elements |> createCsv
-            do! File.WriteAllTextAsync(fileName, csv) |> Async.AwaitTask
+
+            let csv =
+                elements
+                |> Seq.distinctBy (fun x -> x.Href)
+                |> createCsv
+
+            do! File.WriteAllTextAsync(fileName, csv)
+                |> Async.AwaitTask
+
             return ()
         }
-
