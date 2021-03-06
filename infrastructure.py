f99dd8c7e6515f154750399f2e9c42684b652ce5
dominikus1993 <dominikus1910@gmail.com>
2021-03-06 19:15:48 +0000

    small changes

--- infrastructure.py
+++ infrastructure.py
@@ -1,11 +1,12 @@
 
 
 import datetime
+import multiprocessing
 from model import Offer
 from typing import List
 from services import Parser
 
 class FakeParser(Parser): 
     def parse(self, url: str, city: str) -> List[Offer]:
-        print(f'Parser {url} and {city}')
+        print(f'Parser {multiprocessing.current_process()}')
         return [Offer(1.2, ",", "", "", 12.12, 12.2, "", datetime.date(2008, 6, 24))];
