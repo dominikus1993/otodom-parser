5e3048766640901b1386284d66add2cd67ac3e0d
dominikus1993 <dominikus1910@gmail.com>
2021-03-06 17:42:19 +0000

    small changes

---
+++ services.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import List
+from model import Offer
+
+class Parser(ABC):
+
+    @abstractmethod
+    async def parse(self, url: str, city: str) -> List[Offer]:
+        pass
--- usecases.py
+++ usecases.py
@@ -1,13 +1,23 @@
 from datetime import date
 import datetime
+from services import Parser
 from typing import List
 from utils import generate_pages_urls
 from model import Offer
 
-ty
 
-async def load_offer(url: str, cityName: str): List[Offer]
+class GetOffersUseCase:
 
-async def load_offers(url: str, cityName: str, pages: int) -> List[Offer]: 
-    pages_urls = generate_pages_urls(url, pages)
-    return [Offer(1.2, ",", "", "", 12.12, 12.2, "", datetime.date(2008, 6, 24))];
+    offerParser: Parser
+
+    def __init__(self, parser: Parser) -> None:
+        self.offerParser = parser
+
+    
+    async def __load_offer(self, url: str, cityName: str) -> List[Offer]:
+
+        return []
+
+    async def load_offers(self, url: str, cityName: str, pages: int) -> List[Offer]: 
+        pages_urls = generate_pages_urls(url, pages)
+        return [Offer(1.2, ",", "", "", 12.12, 12.2, "", datetime.date(2008, 6, 24))];
