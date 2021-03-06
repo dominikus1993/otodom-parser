7ccedc7e47bbae90abb81362b1c4b6239196116d
dominikus1993 <dominikus1910@gmail.com>
2021-03-06 17:20:50 +0000

    add requirtments.txt

--- usecases.py
+++ usecases.py
@@ -4,6 +4,10 @@ from typing import List
 from utils import generate_pages_urls
 from model import Offer
 
+ty
+
+async def load_offer(url: str, cityName: str): List[Offer]
+
 async def load_offers(url: str, cityName: str, pages: int) -> List[Offer]: 
-    pages_urls = generate_pages_urls(url, pages);
+    pages_urls = generate_pages_urls(url, pages)
     return [Offer(1.2, ",", "", "", 12.12, 12.2, "", datetime.date(2008, 6, 24))];
