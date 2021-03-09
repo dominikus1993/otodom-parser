c01d6998d588da930f81836692ef808f0d038181
dominikus1993 <dominikus1910@gmail.com>
2021-03-09 18:12:07 +0000

    data

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
-import pandas as pd
+import pandas as pd 
+
 
 def calc_avg_price_per_day(data)-> float:
     return data.groupby("Data", as_index=False)["Cena"].mean()
+
+def calc_avg_price_per_meter_per_day(data):
+    return data.groupby("Data", as_index=False)["Cena za m2"].mean()
+
+def calc_avg_price_per_area(data):
+    return data.groupby("Powierzchnia", as_index=False)["Cena"].mean()
