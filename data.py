71e4f5fbb7f90528f05677b6ad0b5441944771d3
dominikus1993 <dominikus1910@gmail.com>
2021-03-09 18:16:52 +0000

    add data

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
+import datetime
 import pandas as pd 
-
 
 def calc_avg_price_per_day(data)-> float:
     return data.groupby("Data", as_index=False)["Cena"].mean()
@@ -9,3 +9,12 @@ def calc_avg_price_per_meter_per_day(data):
 
 def calc_avg_price_per_area(data):
     return data.groupby("Powierzchnia", as_index=False)["Cena"].mean()
+
+
+def get_best_offers(data: pd.DataFrame):
+    today = datetime.datetime.today()
+    dt = data.query(f'Data == "{today}"').query("Powierzchnia > 40 & Powierzchnia < 60").query("Cena > 250000 & Cena < 350000")
+    return dt[~dt["Dzielnica"].str.contains("Górna|Widzew|Śródmieście", na=False)].sort_values(by=['Cena'], ascending=False)
+
+def get_flat_in_specyfic_district(data: pd.DataFrame, district: str):
+    return data[data["Opis"].str.contains(f'{district}|{district.lower()}', na=False)]
