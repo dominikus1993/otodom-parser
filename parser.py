d5a0acf8bdf13fa128713366c2c115d22bddd6b5
dominikus1993 <dominikus1910@gmail.com>
2021-03-09 17:30:44 +0000

    add parser

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+from utils import get_csv_filename
 from infrastructure import FakeStorage, HtmlParser
 from usecases import GetOffersUseCase
 import logging
@@ -15,10 +16,10 @@ logger.addHandler(logHandler)
 OTODOM_URL = "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bregion_id%5D=5&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&search%5Border%5D=created_at_first%3Adesc"
 
 parser = HtmlParser(logger)
-storage = FakeStorage("./data/test.csv", logger)
+storage = FakeStorage(f'./data/{get_csv_filename()}', logger)
 usecase = GetOffersUseCase(parser, storage, logger)
 
-usecase.load_offers(OTODOM_URL, "Łódź", 10)
+usecase.load_offers(OTODOM_URL, "Łódź", 72)
 
 
 logger.info("Finish")
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 from typing import List
+import datetime
 
 def format_price(price: str) -> float:
     result = price.replace(",", ".").replace(" ", "")
     return float(result)
 
+def get_csv_filename() -> str:
+    today = datetime.date.today()
+    return f'otodom-{today}.csv'
+
 def get_district_name(district: str, city_name: str) -> str:
-    return district.replace(f'Mieszkanie na sprzedaż: {city_name} ', "")
+    return district.replace(f'Mieszkanie na sprzedaż: {city_name}, ', "")
 
 def generate_pages_urls(url: str, maxPage: int) -> List[str]:
     return list(map(lambda page: f'{url}&page={page}',range(0, maxPage + 1)))
