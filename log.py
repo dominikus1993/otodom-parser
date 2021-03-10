1614677c1c5f711420ade4a9b8fb998c8eaabea2
dominikus1993 <dominikus1910@gmail.com>
2021-03-10 17:31:05 +0000

    add new data

--- log.py
+++ log.py
@@ -1,11 +1,24 @@
 import logging
+import datetime
 from pythonjsonlogger import jsonlogger
 
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+            
 def get_logger() -> logging.Logger:
     logger = logging.getLogger("otodom.parser")
     logger.setLevel(logging.DEBUG)
     logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
+    formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(name)s %(message)s')
     logHandler.setFormatter(formatter)
     logHandler.setLevel(logging.DEBUG)
 
