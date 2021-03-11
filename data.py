f6b29450ebd039ede028eb642d96a9cd1d8a50e9
dominikus1993 <dominikus1910@gmail.com>
2021-03-11 15:08:39 +0000

    add new data

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import datetime
 import pandas as pd 
+import math
 
 def calc_avg_price_per_day(data)-> float:
     return data.groupby("Data", as_index=False)["Cena"].mean()
@@ -8,7 +9,8 @@ def calc_avg_price_per_meter_per_day(data):
     return data.groupby("Data", as_index=False)["Cena za m2"].mean()
 
 def calc_avg_price_per_area(data):
-    return data.query("Powierzchnia > 40 & Powierzchnia < 60").groupby("Powierzchnia", as_index=False)["Cena"].mean()
+    data["Area"] = data["Powierzchnia"].map(lambda x: math.ceil(float(x)))
+    return data.query("Powierzchnia > 40 & Powierzchnia < 60").groupby("Area", as_index=False)["Cena"].mean()
 
 
 def get_best_offers(data):
