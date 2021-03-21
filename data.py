ec490b6c83dacb9be1b570e800ce0cc2a5552136
dominikus1993 <dominikus1910@gmail.com>
2021-03-21 10:01:12 +0000

    add data

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ def calc_avg_price_per_area(data):
 
 def get_best_offers(data):
     today = datetime.date.today()
-    dt = data.query(f'Data == "{today}"').query("Powierzchnia > 40 & Powierzchnia < 60").query("Cena > 250000 & Cena < 350000").drop_duplicates()
+    dt = data.query(f'Data == "{today}"').query("Powierzchnia > 30 & Powierzchnia < 50").query("Cena > 200000 & Cena < 350000").drop_duplicates()
     return dt[~dt["Dzielnica"].str.contains("Górna|Widzew|Śródmieście", na=False)].sort_values(by=['Cena'], ascending=False)
 
 def get_flat_in_specyfic_district(data: pd.DataFrame, district: str):
--- parser.py
+++ parser.py
@@ -5,13 +5,13 @@ import log
 
 logger = log.get_logger()
 
-OTODOM_URL = "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bregion_id%5D=5&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&search%5Border%5D=created_at_first%3Adesc"
+OTODOM_URL = "https://www.otodom.pl/sprzedaz/mieszkanie/lodz/?search%5Bfilter_enum_market%5D=secondary&search%5Bfilter_float_build_year%3Ato%5D=2019&search%5Border%5D=created_at_first%3Adesc&search%5Bregion_id%5D=5&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004"
 
 parser = HtmlParser(logger)
 storage = CsvStorage(f'./data/{get_csv_filename()}', logger)
 usecase = GetOffersUseCase(parser, storage, logger)
 
-usecase.load_offers(OTODOM_URL, "Łódź", 72)
+usecase.load_offers(OTODOM_URL, "Łódź", 60)
 
 
 logger.info("Finish")
